dde89a9a8a1eca5c14a8b8d5ac97f673101664d8
seweed3 <matthewc1172@gmail.com>
2025-04-30 20:56:34 -0700

    Servo Bounds Setter and Testing for Arm Mechanism

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/ServoBoundsSetter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/ServoBoundsSetter.java
@@ -13,114 +13,181 @@ import org.firstinspires.ftc.teamcode.MecanumDrive;
 @TeleOp
 public class ServoBoundsSetter extends OpMode {
 
-    Servo leftArmServo;
-    Servo rightArmServo;
-    Servo sampleClaw;
-    Servo specimenClaw;
-    Servo wristServo;
+    Servo leftArm;
+    Servo rightArm;
+    Servo leftPivotingSlides;
+    Servo rightPivotingSlides;
+    Servo leftWrist;
+    Servo rightWrist;
     Servo turnTable;
-
+    Servo leftSpintake;
+    Servo rightSpintake;
+    Servo specimenClaw;
     Timer timer;
 
     @Override
     public void init() {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
-        sampleClaw = hardwareMap.get(Servo.class, "Sample Claw");
-        specimenClaw = hardwareMap.get(Servo.class, "Specimen Claw");
-        leftArmServo = hardwareMap.get(Servo.class, "Arm Left");
-        rightArmServo = hardwareMap.get(Servo.class, "Arm Right");
-        wristServo  = hardwareMap.get(Servo.class, "Wrist");
-        turnTable = hardwareMap.get(Servo.class, "Turn Table");
 
+//        leftArm = hardwareMap.get(Servo.class, "Arm Left");
+//        rightArm = hardwareMap.get(Servo.class, "Arm Right");
+        leftPivotingSlides = hardwareMap.get(Servo.class, "PivotingSlides Left");
+        rightPivotingSlides = hardwareMap.get(Servo.class, "PivotingSlides Right");
+//        leftWrist = hardwareMap.get(Servo.class, "Wrist Left");
+//        rightWrist = hardwareMap.get(Servo.class, "Wrist Right");
+//        turnTable = hardwareMap.get(Servo.class, "Turn Table");
+//        leftSpintake = hardwareMap.get(Servo.class, "Spintake Left");
+//        rightSpintake = hardwareMap.get(Servo.class, "Spintake Right");
+//        specimenClaw = hardwareMap.get(Servo.class, "Specimen Claw");
+
+
+//        rightSpintake.setDirection(Servo.Direction.REVERSE);
+        leftPivotingSlides.setDirection(Servo.Direction.REVERSE);
         timer = new Timer();
     }
 
     boolean manualControl = false;
-    String activeServo = "Wrist";
+    String activeServo = "Dual Pivoting Slides";
     double servoPosition = 0.5;
 
 
     @Override
     public void loop() {
+        telemetry.addLine("Gamepad 1");
         telemetry.addLine("A - Left Arm");
         telemetry.addLine("X - Right Arm");
-        telemetry.addLine("Y - Sample Claw");
-        telemetry.addLine("B - Wrist");
-        telemetry.addLine("Right Bumper - Dual Arm");
+        telemetry.addLine("Y - Dual Arm");
+        telemetry.addLine("B - Right Pivoting Slides");
+
+        //DPADS - Pivoting Slides and Wrist - Individual
+        telemetry.addLine("Dpad Left - Left Wrist");
+        telemetry.addLine("Dpad Right - Right Wrist");
+
+
         telemetry.addLine("Left Bumper - Turn Table");
         telemetry.addLine("Left Trigger - Specimen Claw");
+        telemetry.addLine("Right Bumper - Dual Spintake");
+        telemetry.addLine("Right Trigger - Left Pivoting Slides");
+        telemetry.addLine("Gamepad 2.A - Dual Pivoting Slides");
+        telemetry.addLine("Back - Left Spintake");
+        telemetry.addLine("Start - Right Spintake");
 
+        telemetry.addLine("Dpad up - Increase");
+        telemetry.addLine("Dpad down - Decrease");
+
+
+        //Assign active servo
         if (gamepad1.a) {
-            activeServo = "Left arm";
+            activeServo = "Left Arm";
         }
         if (gamepad1.x) {
-            activeServo = "Right arm";
+            activeServo = "Right Arm";
         }
         if (gamepad1.y) {
-            activeServo = "Sample Claw";
-        }
-        if(gamepad1.b){
-            activeServo = "Wrist";
-        }
-        if(gamepad1.right_bumper){
             activeServo = "Dual Arm";
         }
+        if(gamepad1.b){
+            activeServo = "Right Pivoting Slides";
+        }
+        if(gamepad2.a){
+            activeServo = "Dual Pivoting Slides";
+        }
+
+        if(gamepad1.dpad_left){
+            activeServo = "Left Wrist";
+        }
+        if(gamepad1.dpad_right){
+            activeServo = "Right Wrist";
+        }
+
+
         if(gamepad1.left_bumper){
             activeServo = "Turn Table";
         }
         if(gamepad1.left_trigger > 0.5){
             activeServo = "Specimen Claw";
         }
+        if(gamepad1.right_bumper){
+            activeServo = "Dual Spintake";
+        }
+        if(gamepad1.right_trigger > 0.5){
+            activeServo = "Left Pivoting Slides";
+        }
+
+        if(gamepad1.back){
+            activeServo = "Left Spintake";
+
+        }
+        else if(gamepad1.start){
+            activeServo = "Right Spintake";
+        }
+
+        //Set values to servo posiiton
+        if(activeServo.equals("Left Arm")){
+            leftArm.setPosition(servoPosition);
+        }
+
+
+        else if(activeServo.equals("Right Arm")){
+            rightArm.setPosition(servoPosition);
+        }
+        else if(activeServo.equals("Dual Arm")){
+            leftArm.setPosition(servoPosition);
+            rightArm.setPosition(servoPosition);
+        }
+
+        else if(activeServo.equals("Right Pivoting Slides")){
+            rightPivotingSlides.setPosition(servoPosition);
+        }
+        else if(activeServo.equals("Left Pivoting Slides")){
+            leftPivotingSlides.setPosition(servoPosition);
+        }
+        else if(activeServo.equals("Dual Pivoting Slides")){
+            leftPivotingSlides.setPosition(servoPosition);
+            rightPivotingSlides.setPosition(servoPosition);
+        }
+
+        else if(activeServo.equals("Left Wrist")){
+            leftWrist.setPosition(servoPosition);
+        }
+        else if(activeServo.equals("Right Wrist")){
+            rightWrist.setPosition(servoPosition);
+        }
+        else if(activeServo.equals("Turn Table")){
+            turnTable.setPosition(servoPosition);
+        }
+        else if(activeServo.equals("Specimen Claw")){
+            specimenClaw.setPosition(servoPosition);
+        }
+        else if(activeServo.equals("Dual Spintake")){
+            leftSpintake.setPosition(servoPosition);
+            rightSpintake.setPosition(servoPosition);
+        }
+        else if(activeServo.equals("Left Spintake")){
+            leftSpintake.setPosition(servoPosition);
+        }
+        else if(activeServo.equals("Right Spintake")){
+            rightSpintake.setPosition(servoPosition);
+        }
+
+
 
         timer.updateTime();
-
-        if (gamepad1.dpad_up) {
-            manualControl = false;
+        if(gamepad1.dpad_up){
             servoPosition += 0.25 * timer.getDeltaTime();
         }
 
-        else if (gamepad1.dpad_down) {
-            manualControl = false;
+        if(gamepad1.dpad_down){
             servoPosition -= 0.25 * timer.getDeltaTime();
         }
 
-        else if(gamepad1.dpad_left){
-            manualControl = true;
-            leftArmServo.setPosition(0);
-            rightArmServo.setPosition(0);
-        }
 
-        else if(gamepad1.dpad_right){
-            manualControl = true;
-            leftArmServo.setPosition(0.6);
-            rightArmServo.setPosition(0.6);
-        }
 
-        if(!manualControl) {
-            if (activeServo.equals("Left arm")) {
-                leftArmServo.setPosition((servoPosition));
-            } else if (activeServo.equals("Right arm")) {
-                rightArmServo.setPosition(servoPosition);
-            } else if (activeServo.equals("Wrist")) {
-                wristServo.setPosition(servoPosition);
-            } else if (activeServo.equals("Sample Claw")) {
-                sampleClaw.setPosition(servoPosition);
-            } else if(activeServo.equals("Specimen Claw")){
-                specimenClaw.setPosition(servoPosition);
-            }
-            else if(activeServo.equals("Turn Table")){
-                turnTable.setPosition(servoPosition);
-            }
-            else if(activeServo.equals("Dual Arm")) {
-                leftArmServo.setPosition(servoPosition);
-                rightArmServo.setPosition(servoPosition);
-            }
-        }
+
 
         telemetry.addData("Dpad Right", gamepad1.dpad_right);
         telemetry.addData("Dpad Up", gamepad1.dpad_up);
-        telemetry.addData("Dpad Left", gamepad1.dpad_left);
         telemetry.addData("Active servo", activeServo);
         telemetry.addData("Servo position", servoPosition);
         telemetry.update();
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/ActionsTestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/ActionsTestOpMode.java
@@ -5,13 +5,18 @@ import android.annotation.SuppressLint;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.Autonomous.FourSampleAuto;
 import org.firstinspires.ftc.teamcode.Components.Timer;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.Mechanisms.Arm;
+import org.firstinspires.ftc.teamcode.Mechanisms.PivotingSlides;
 import org.firstinspires.ftc.teamcode.Mechanisms.SampleClaw;
 import org.firstinspires.ftc.teamcode.Mechanisms.SpecimenClaw;
 import org.firstinspires.ftc.teamcode.Mechanisms.TurnTable;
@@ -21,41 +26,54 @@ import org.firstinspires.ftc.teamcode.Mechanisms.Wrist;
 
 @TeleOp(group="Test")
 public class ActionsTestOpMode extends OpMode {
+    RevColorSensorV3 outsideColorSensor;
+    RevColorSensorV3 insideColorSensor;
+    Servo leftSpintake;
+    Servo rightSpintake;
 
     SampleClaw sampleClaw;
     TurnTable turnTable;
     VerticalSlides slides;
     MecanumDrive mecanumDrive;
     Pose2d initialPose;
-    Wrist wrist;
+//    Wrist wrist;
     Arm arm;
     SpecimenClaw specimenClaw;
+    PivotingSlides pivotingSlides;
     Timer timer;
     @Override
     public void init() {
-        initialPose = new Pose2d(0, 0, 0);
-        mecanumDrive = new MecanumDrive(hardwareMap, initialPose);
-        sampleClaw = new SampleClaw(hardwareMap);
-        specimenClaw = new SpecimenClaw(hardwareMap);
-        wrist = new Wrist(hardwareMap);
+//        initialPose = new Pose2d(0, 0, 0);
+//        mecanumDrive = new MecanumDrive(hardwareMap, initialPose);
+//        sampleClaw = new SampleClaw(hardwareMap);
+//        specimenClaw = new SpecimenClaw(hardwareMap);
+//        wrist = new Wrist(hardwareMap);
         arm = new Arm(hardwareMap);
-        turnTable = new TurnTable(hardwareMap);
+//        turnTable = new TurnTable(hardwareMap);
         timer = new Timer();
 
-        slides = new VerticalSlides(hardwareMap);
+//        slides = new VerticalSlides(hardwareMap);
         arm.setPositionDirect(Arm.STRAIGHT_UP_POSITION);
-        wrist.setInitPosition();
-        sampleClaw.release();
-        turnTable.setPosition(TurnTable.NEUTRAL_POS);
-        slides.resetEncoders();
+        pivotingSlides = new PivotingSlides(hardwareMap);
+//        wrist.setInitPosition();
+//        sampleClaw.release();
+//        turnTable.setPosition(TurnTable.NEUTRAL_POS);
+//        slides.resetEncoders();
+
+        leftSpintake = hardwareMap.get(Servo.class, "Left Spintake");
+        rightSpintake = hardwareMap.get(Servo.class, "Right Spintake");
+        rightSpintake.setDirection(Servo.Direction.REVERSE);
+
+//        outsideColorSensor = hardwareMap.get(RevColorSensorV3.class, "outsideColorSensor");
+        insideColorSensor = hardwareMap.get(RevColorSensorV3.class, "insideColorSensor");
 
         telemetry.setAutoClear(true);
     }
 
-    @Override
-    public void start() {
-        mecanumDrive.localizer.setPose(FourSampleAuto.startingPose);
-    }
+//    @Override
+//    public void start() {
+//        mecanumDrive.localizer.setPose(FourSampleAuto.startingPose);
+//    }
 
 
 
@@ -67,21 +85,91 @@ public class ActionsTestOpMode extends OpMode {
         return new SequentialAction();
     }
 
-    @SuppressLint("DefaultLocale")
+//    @SuppressLint("DefaultLocale")
+    double extensionLength = 9;
+    double speed = 1;
+    final double INTAKE_THRESHOLD  = 0.6;
+    final double OUTTAKE_THRESHOLD = 2;
+
+    boolean intaking;
+    boolean outtaking;
     @Override
     public void loop() {
-//        timer.updateTime();
+
+        timer.updateTime();
+        double deltaTime = timer.getDeltaTime();
 //
-//        double armPos = arm.leftServo.getPosition() + gamepad1.left_stick_y * timer.getDeltaTime() * 0.5;
+
+        double armPos = arm.leftServo.getPosition() + gamepad1.left_stick_y * deltaTime * 0.5;
 //        double wristPos = calculateWristPosition(armPos, endEffectorOrientation);
 //
 //        endEffectorOrientation += gamepad1.right_stick_y * timer.getDeltaTime() * 0.5;
 //
 //        drive.updatePoseEstimate();
-//        arm.setPositionDirect(armPos);
+        if(gamepad1.left_bumper) {
+            arm.setPositionDirect(armPos);
+        }
+        else if(gamepad1.x){
+            Actions.runBlocking(arm.setPositionSmooth(Arm.SCANNING_POSITION));
+        }
+        else if(gamepad1.y){
+            Actions.runBlocking(arm.setPositionSmooth(Arm.STRAIGHT_UP_POSITION));
+        }
+
+        if(gamepad1.dpad_left){
+            intaking = true;
+        }
+        if(gamepad1.dpad_right){
+            outtaking = true;
+        }
+
+
+        if (intaking) {
+            leftSpintake.setPosition(1);
+            rightSpintake.setPosition(1);
+            intaking = (insideColorSensor.getDistance(DistanceUnit.INCH) > INTAKE_THRESHOLD);
+        }
+        else if (outtaking) {
+            leftSpintake.setPosition(0);
+            rightSpintake.setPosition(0);
+            outtaking = (insideColorSensor.getDistance(DistanceUnit.INCH) < OUTTAKE_THRESHOLD);
+        }
+        else {
+            leftSpintake.setPosition(0.5);
+            rightSpintake.setPosition(0.5);
+        }
+
+
+
+
 //        wrist.setPositionDirect(wristPos);
 //
-//        telemetry.addData("ArmPos", armPos);
+
+        extensionLength += gamepad1.dpad_up ? deltaTime * speed : 0;
+        extensionLength -= gamepad1.dpad_down ? deltaTime * speed : 0;
+
+        if (gamepad1.a) pivotingSlides.setExtensionLength(extensionLength);
+        if (gamepad1.b) pivotingSlides.setExtensionLength(0);
+
+
+
+
+        telemetry.addData("ArmPos", armPos);
+        telemetry.addData("Extension Length", extensionLength);
+        
+        telemetry.addData("Left Servo Pos", leftSpintake.getPosition());
+        telemetry.addData("Right Servo Pos", rightSpintake.getPosition());
+        telemetry.addData("Outside color sensor distance", outsideColorSensor.getDistance(DistanceUnit.INCH));
+        telemetry.addData("Inside color sensor distance", insideColorSensor.getDistance(DistanceUnit.INCH));
+
+        telemetry.addData("Inside color sensor optical", insideColorSensor.getRawLightDetected());
+        telemetry.addData("Outside color sensor optical", outsideColorSensor.getRawLightDetected());
+
+        telemetry.addData("Inside color sensor raw", insideColorSensor.rawOptical());
+        telemetry.addData("outside color sensor raw", outsideColorSensor.rawOptical());
+
+        telemetry.addData("Time", timer.updateTime());
+
 //        telemetry.addData("Arm orientation", Math.toDegrees(Kinematics.armPositionToOrientation(armPos)));
 //        telemetry.addData("WristPos", wristPos);
 //        telemetry.addData("Wrist orientation", Math.toDegrees(Kinematics.wristPositionToOrientation(wristPos)));
@@ -93,6 +181,6 @@ public class ActionsTestOpMode extends OpMode {
 //        telemetry.addData("End effector pose", String.format("(%f, %f)", endEffectorPose.position.x, endEffectorPose.position.y));
 //        telemetry.addData("End effector rotation", Math.toDegrees(endEffectorPose.heading.toDouble()));
 //
-//        telemetry.update();
+        telemetry.update();
     }
 }
