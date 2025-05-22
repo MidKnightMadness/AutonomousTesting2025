8c8eac0e366debcb46bf22bea8d042f37bce1a5e
seweed3 <matthewc1172@gmail.com>
2025-05-22 01:44:49 -0700

    Sample Object Fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Sample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Sample.java
@@ -9,37 +9,39 @@ public class Sample {
     private double relativeX; //Center
     private double relativeY; //Center
 
-    private double worldX;
-    private double worldY;
+//    private double worldX;
+//    private double worldY;
     private double confidence;
-    private Sample.Rotation relativesSampleRotation;
-    private InverseKinematics.IKResult IKR;
+    private double hyp;
+    private double sampleTheta;
+//    private InverseKinematics.IKResult IKR;
     public enum Rotation{
         Vertical,
         Horizontal,
         None
     }
 
-    public Sample(SampleColors color, double[] relativeCoordinates, double[] worldCoordinates, Sample.Rotation relativesSampleRotation, double confidence, InverseKinematics.IKResult ikResult){
+    public Sample(SampleColors color, double[] relativeCoordinates, double hyp, double sampleTheta, double confidence){
         this.color = color;
         this.relativeX = relativeCoordinates[0];
         this.relativeY = relativeCoordinates[1];
-        this.worldX = worldCoordinates[0];
-        this.worldY = worldCoordinates[1];
+//        this.worldX = worldCoordinates[0];
+//        this.worldY = worldCoordinates[1];
+        this.hyp = hyp;
+        this.sampleTheta = sampleTheta;
         this.confidence = confidence;
-        this.relativesSampleRotation = relativesSampleRotation;
-        this.IKR = ikResult;
     }
 
     public Sample(){
         this.color = SampleColors.NONE;
         this.relativeX = 0;
         this.relativeY = 0;
-        this.worldX = 0;
-        this.worldY = 0;
+//        this.worldX = 0;
+//        this.worldY = 0;
+        this.hyp = 0;
+        this.sampleTheta = 0;
         this.confidence = 0;
-        this.relativesSampleRotation = Rotation.Vertical;
-        this.IKR = new InverseKinematics.IKResult();
+//        this.IKR = new InverseKinematics.IKResult();
     }
 
     public SampleColors getColor(){
@@ -53,23 +55,27 @@ public class Sample {
         return relativeY;
     }
 
-    public double getWorldX(){
-        return worldX;
+//    public double getWorldX(){
+//        return worldX;
+//    }
+//    public double getWorldY(){
+//        return worldY;
+//    }
+
+    public double getSampleDistance(){
+        return hyp;
     }
-    public double getWorldY(){
-        return worldY;
+    public double getSampleTheta(){//Radians
+        return sampleTheta;
     }
+
 
     public double getConfidence(){
         return confidence;
     }
 
-    public Sample.Rotation getSampleRotation(){
-        return relativesSampleRotation;
-    }
-
-    public InverseKinematics.IKResult getIKRResult(){
-        return IKR;
-    }
+//    public InverseKinematics.IKResult getIKRResult(){
+//        return IKR;
+//    }
 
 }
